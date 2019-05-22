cb4caad582c83d3abfd0077e9d111421b13ee48c
rahul_excell <rahul.excel2011@gmail.com>
2019-05-22 19:01:44 +0530

    changed hashrouter to browserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.scss";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
